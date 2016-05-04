177bd4fbdafebdb6644a0bb3cfce8afaf202baaa
Yura Zinets <yura.zinets@gmail.com>
2016-05-04 20:25:36 +0930

    Added JQuery, removed needless methods from DB, added function to DB!

--- src/DatabaseBundle/DB.php
+++ src/DatabaseBundle/DB.php
@@ -32,8 +32,12 @@ class DB {
 
     }
 
+    public function selectCurrentTemperatures()
+    {
+        $sql = "SELECT * FROM temperatures WHERE time=max_time()";
+        $request = $this->connection->executeQuery($sql);
+        $request_result = $request->fetchAll();
 
-    public function normalizeResult($request_result) {
         $result = array();
         foreach ($request_result as $row) {
             if (!array_key_exists($row['controller'], $result)) {
@@ -43,34 +47,6 @@ class DB {
         }
         return $result;
     }
-
-
-    // Remake this shit
-
-    public function selectControllersWithTypes()
-    {
-        $sql = "SELECT DISTINCTROW controller, value_type FROM temperatures";
-        $request = $this->connection->executeQuery($sql);
-        return $request->fetchAll();
-    }
-
-
-    public function selectCurrentTemperatures()
-    {
-        $request_result = array();
-        $sql = "SELECT * FROM temperatures WHERE controller=:controller AND value_type=:type ORDER BY time DESC LIMIT 1";
-        foreach ($this->selectControllersWithTypes() as $row) {
-            $request = $this->connection->prepare($sql);
-            $request->bindValue("controller", $row["controller"]);
-            $request->bindValue("type", $row["value_type"]);
-            $request->execute();
-            array_push($request_result, $request->fetch());
-        }
-
-        return $this->normalizeResult($request_result);
-    }
-    
-    
 
     public function selectAllControllerTemperatures(string $controller) {
         $sql = "SELECT * FROM temperatures WHERE controller=:controller";
